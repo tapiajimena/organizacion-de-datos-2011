10f88b241f1a15273033cea5fe2faa185e53f873
paulofer85@gmail.com <paulofer85@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-07 04:21:24 +0000

    importa libros a la biblioteca y los puede imprimir.

--- trunk/TPDatos/src/ModuloDeArchivos/ArchivoVariable.cpp
+++ trunk/TPDatos/src/ModuloDeArchivos/ArchivoVariable.cpp
@@ -45,20 +45,14 @@ void ArchivoVariable::escribir(RegistroVariable & rv, uint32_t offset)
 }
 
 
-/**
- * escribe el contenido al final del archivo prueba.dat abriendo y cerrando el archivo
- */
+
 void ArchivoVariable::escribirAlFinal(RegistroVariable &rv)
 {
 	uint32_t size 		= rv.getDato().getSize();
 
-	//arcVariable.open("prueba.dat", ios::in | ios::out |ios::binary|ios::ate);
-	//archivoVariable.seekp(0, ios_base::end);
-
 	archivoVariable.write(reinterpret_cast<char *> (&size), sizeof(size));
 	archivoVariable.write(rv.getDato().toCharPointer(), size);
 	archivoVariable.flush();
-	//arcVariable.close();
 }
 
 
@@ -67,15 +61,50 @@ void ArchivoVariable::irAInicio()
 	this->archivoVariable.seekp(0, ios_base::beg);
 }
 
-char* ArchivoVariable::leer()
+
+char* ArchivoVariable::leer(fstream & arcLibro, uint32_t & size)
+{
+	char* contenidoLibro = (char*)(malloc(0));
+
+	size  = getSizeArchivo(arcLibro);
+    arcLibro.seekp(0, ios_base::beg);
+
+    //se lee
+    contenidoLibro = (char*)(realloc(contenidoLibro, size));
+    arcLibro.read(contenidoLibro, size);
+    return contenidoLibro;
+}
+
+void ArchivoVariable::agregarLibro(char* pathLibro)
+{
+	string rdo;
+	Datos dato;
+	uint32_t sizeAux;
+	fstream arcLibro(pathLibro, ios::in | ios::binary);
+
+	//se lee en contenidoLibro el libro completo
+    char* contenidoLibro = leer(arcLibro, sizeAux);
+
+	//se procesa
+	rdo = contenidoLibro;
+	rdo = rdo.substr(0,sizeAux);
+	dato.setDatos(rdo);
+	RegistroVariable* rv = new RegistroVariable(dato);
+
+	arcLibro.close();//se cierra el arcLibro
+	escribirAlFinal(*rv);//se escribe en la biblioteca
+	delete (rv);
+	arcLibro.close();
+}
+
+
+char* ArchivoVariable::leerRegistroVariable()
 {
 	string rdo ="";
 	uint32_t size = 0;
 	char* contenido	= (char*)malloc(0);
 
-	//arcVariable.seekp(offset);
 	archivoVariable.read(reinterpret_cast<char *>(&size), sizeof(size));
-
 	contenido = (char*)realloc(contenido, size);
 	archivoVariable.read(contenido, size);
 
@@ -84,9 +113,17 @@ char* ArchivoVariable::leer()
 	return contenido;
 }
 
-uint32_t ArchivoVariable::getSizeArchivo()
+
+uint32_t ArchivoVariable::getSizeArchivo(fstream arc)
 {
-	return this->archivoVariable.tellg();//pide cual es el tamanio del archivo
+    arc.seekp(0, ios_base::end);
+	return arc.tellg();//pide cual es el tamanio del archivo
+}
+
+uint32_t ArchivoVariable::getSizeArchivoVariable()
+{
+    archivoVariable.seekp(0, ios_base::end);
+	return archivoVariable.tellg();//pide cual es el tamanio del archivo
 }
 
 bool ArchivoVariable::finArchivo()
