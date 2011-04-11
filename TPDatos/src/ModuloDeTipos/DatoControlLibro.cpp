fa4cf3953cd6ee0ade7ce2f6e5bcf1d428b08181
paulofer85@gmail.com <paulofer85@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-11 03:09:10 +0000

    Modifcaciones + Nuevo tipo de DatoControlLibro

---
+++ TPDatos/src/ModuloDeTipos/DatoControlLibro.cpp
@@ -0,0 +1,22 @@
+/*
+ * DatoControlLibro.cpp
+ *  
+ *	Autor	: GRUPO 1 - Fernandez, Gallinal, Maraggi, Tapia
+ *	Catedra	: SERVETTO-FERRER-FERNANDEZ
+ *	Materia	: Organizacion de Datos (75.06) - FIUBA
+ *      
+ *  
+ *      
+ *      
+ */
+
+#include "DatoControlLibro.h"
+
+DatoControlLibro::DatoControlLibro() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DatoControlLibro::~DatoControlLibro() {
+	// TODO Auto-generated destructor stub
+}
