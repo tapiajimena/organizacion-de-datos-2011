5857d9705de150b5444ff4f714a012b5a51b1189
paulofer85@gmail.com <paulofer85@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-12 07:18:29 +0000

    en implementacion NodoINterno

--- TPDatos/src/ModuloDeIndices/ArbolBMas/src/NodoArbol.cpp
+++ TPDatos/src/ModuloDeIndices/ArbolBMas/src/NodoArbol.cpp
@@ -13,6 +13,15 @@ void NodoArbol::setId(int id) {
 	this->id = id;
 }
 
+list<int> getHijos() const
+{
+	return NULL;
+}
+list<string> getClaves() const
+{
+	return NULL;
+}
+
 NodoArbol::~NodoArbol() {
 	// TODO Auto-generated destructor stub
 }
