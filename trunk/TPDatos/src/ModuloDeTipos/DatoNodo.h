42c5c7696607ce989becea243e4747a156def509
paulofer85@gmail.com <paulofer85@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-12 07:19:58 +0000

    en implementacion DatoNodo

---
+++ trunk/TPDatos/src/ModuloDeTipos/DatoNodo.h
@@ -0,0 +1,46 @@
+/*
+ * DatoNodo.h
+ *  
+ *	Autor	: GRUPO 1 - Fernandez, Gallinal, Maraggi, Tapia
+ *	Catedra	: SERVETTO-FERRER-FERNANDEZ
+ *	Materia	: Organizacion de Datos (75.06) - FIUBA
+ *      
+ *  
+ *      
+ *      
+ */
+
+#ifndef DATONODO_H_
+#define DATONODO_H_
+
+/*
+ *
+ */
+#include "Dato.h"
+#include "../ModuloDeIndices/ArbolBMas/src/NodoInternoArbol.h"
+#include <list>
+
+
+
+class DatoNodo: public Dato {
+
+private:
+	//TODO Implementar cuando un nodo no entra en un bloque.
+	uint32_t offset_continuacion;
+
+	int cantidad_hijos;
+	list<int> hijos;
+	int size_claves;
+	string claves;
+
+
+public:
+
+	DatoNodo();
+
+	DatoNodo(NodoInternoArbol* nodo);
+
+	virtual ~DatoNodo();
+};
+
+#endif /* DATONODO_H_ */
