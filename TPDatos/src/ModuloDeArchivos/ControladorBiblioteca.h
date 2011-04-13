9125c1ff0179c21f0635735a65fe89db5db4310e
paulofer85@gmail.com <paulofer85@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-13 06:32:33 +0000

    Creacion de la clase ControladorBiblioteca

---
+++ TPDatos/src/ModuloDeArchivos/ControladorBiblioteca.h
@@ -0,0 +1,58 @@
+/*
+ * ControladorBiblioteca.cpp
+ *
+ *	Autor	: GRUPO 1 - Fernandez, Gallinal, Maraggi, Tapia
+ *	Catedra	: SERVETTO-FERRER-FERNANDEZ
+ *	Materia	: Organizacion de Datos (75.06) - FIUBA
+ *
+ *
+ *
+ *
+ */
+
+#ifndef CONTROLADORBIBLIOTECA_H_
+#define CONTROLADORBIBLIOTECA_H_
+
+#include "ArchivoLibro.h"
+#include "ArchivoControlLibro.h"
+
+class ControladorBiblioteca {
+private:
+	ArchivoLibro* arcLibro;
+	ArchivoControlLibro* arcControl;
+
+public:
+	ControladorBiblioteca();
+
+	/*
+	 * crea el ArchivoLibro y su archivo de control, con los path indicados.
+	 * @param pathBiblioteca path del archivo de biblioteca
+	 * @param pathControlBiblioteca path de control de la biblioteca
+	 */
+	ControladorBiblioteca(string pathBiblioteca, string pathControlBiblioteca);
+
+
+	/*
+	 * agrega un libro a la biblioteca y su data de control en el archivo de control
+	 * @param pathLibro ruta del libro a agregar
+	 */
+	bool agregarLibro(string pathLibro);
+
+
+	/*
+	 * elimina un libro a la biblioteca y su data de control en el archivo de control
+	 * @param offset id del libro a eliminar
+	 */
+	bool eliminarLibro(uint32_t offset);
+
+
+	string getPathBiblioteca();
+	string getPathControlBiblioteca();
+
+
+
+
+	virtual ~ControladorBiblioteca();
+};
+
+#endif /* CONTROLADORBIBLIOTECA_H_ */
