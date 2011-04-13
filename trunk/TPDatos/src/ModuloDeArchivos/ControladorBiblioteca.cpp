61724bce02e4603e9595521a01aae4a095401a7e
paulofer85@gmail.com <paulofer85@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-13 06:32:33 +0000

    Creacion de la clase ControladorBiblioteca

---
+++ trunk/TPDatos/src/ModuloDeArchivos/ControladorBiblioteca.cpp
@@ -0,0 +1,55 @@
+/*
+ * ControladorBiblioteca.cpp
+ *
+ *	Autor	: GRUPO 1 - Fernandez, Gallinal, Maraggi, Tapia
+ *	Catedra	: SERVETTO-FERRER-FERNANDEZ
+ *	Materia	: Organizacion de Datos (75.06) - FIUBA
+ *
+ *
+ *
+ *
+ */
+
+#include "ControladorBiblioteca.h"
+
+
+ControladorBiblioteca::ControladorBiblioteca()
+{
+
+
+}
+
+ControladorBiblioteca::ControladorBiblioteca(string pathBiblioteca, string pathControlBiblioteca)
+{
+    arcLibro = new ArchivoLibro(pathBiblioteca);
+    arcControl = new ArchivoControlLibro(pathControlBiblioteca);
+}
+
+bool ControladorBiblioteca::agregarLibro(string pathLibro)
+{
+	arcControl->registrarLibro(arcLibro->getPosicionActual());
+	arcLibro->agregarLibro((char*)pathLibro.c_str());
+}
+
+
+bool ControladorBiblioteca::eliminarLibro(uint32_t offset)
+{
+	//TODO actualizar el estado del control del archivo
+	arcLibro->eliminar(offset);
+}
+
+string ControladorBiblioteca::getPathControlBiblioteca()
+{
+	return arcControl->getPathArchivoControlLibro();
+}
+
+string ControladorBiblioteca::getPathBiblioteca()
+{
+	return arcLibro->getPath();
+}
+
+
+ControladorBiblioteca::~ControladorBiblioteca() {
+	delete arcLibro;
+	delete arcControl;
+}
