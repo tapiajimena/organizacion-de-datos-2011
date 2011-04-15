d1fc576fb1ef1191255a007df363401f11dbde31
jimena.soledad.tapia <jimena.soledad.tapia@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-15 06:03:47 +0000

    nueva clase DatoControlLibro

--- TPDatos/src/ModuloDeTipos/DatoControlLibro.cpp
+++ TPDatos/src/ModuloDeTipos/DatoControlLibro.cpp
@@ -14,7 +14,55 @@
 
 DatoControlLibro::DatoControlLibro() {
 	// TODO Auto-generated constructor stub
+}
 
+DatoControlLibro::DatoControlLibro(uint32_t id_Libro, uint32_t espacioLibre,
+			list<char>* indexado, bool modificado, uint32_t offset){
+	this->espacioLibre = espacioLibre;
+	this->id_Libro = id_Libro;
+	this->indexado = indexado;
+	this->modificado = modificado;
+	this->offset = offset;
+}
+
+uint32_t DatoControlLibro::getEspacioLibre() const {
+	return espacioLibre;
+}
+
+uint32_t DatoControlLibro::getId_Libro() const {
+	return id_Libro;
+}
+
+list<char> * DatoControlLibro::getIndexado() const {
+	return indexado;
+}
+
+bool DatoControlLibro::getModificado() const {
+	return modificado;
+}
+
+uint32_t DatoControlLibro::getOffset() const {
+	return offset;
+}
+
+void DatoControlLibro::setEspacioLibre(uint32_t espacioLibre) {
+	this->espacioLibre = espacioLibre;
+}
+
+void DatoControlLibro::setId_Libro(uint32_t id_Libro) {
+	this->id_Libro = id_Libro;
+}
+
+void DatoControlLibro::setIndexado(list<char> *indexado) {
+	this->indexado = indexado;
+}
+
+void DatoControlLibro::setModificado(bool modificado) {
+	this->modificado = modificado;
+}
+
+void DatoControlLibro::setOffset(uint32_t offset) {
+	this->offset = offset;
 }
 
 DatoControlLibro::~DatoControlLibro() {
