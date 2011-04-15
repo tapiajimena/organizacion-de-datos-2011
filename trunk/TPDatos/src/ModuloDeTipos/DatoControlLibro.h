c7b94a2cad013916c79c89cc07a03fabdd99aedf
jimena.soledad.tapia <jimena.soledad.tapia@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-15 06:03:47 +0000

    nueva clase DatoControlLibro

--- trunk/TPDatos/src/ModuloDeTipos/DatoControlLibro.h
+++ trunk/TPDatos/src/ModuloDeTipos/DatoControlLibro.h
@@ -13,16 +13,36 @@
 #ifndef DATOCONTROLLIBRO_H_
 #define DATOCONTROLLIBRO_H_
 
-/*
- *
- */
+#include <list>
+#include <stdint.h>
+
 #include "Dato.h"
 
+using namespace std;
+
 class DatoControlLibro: public Dato {
+private:
+	uint32_t id_Libro;
+	uint32_t espacioLibre;
+	list<char>* indexado;
+	bool modificado;
+	uint32_t offset;
 public:
 	DatoControlLibro();
+	DatoControlLibro(uint32_t id_Libro, uint32_t espacioLibre,
+			list<char>* indexado, bool modificado, uint32_t offset);
 
-
+	/* Getters y Setters */
+	uint32_t getEspacioLibre() const;
+	uint32_t getId_Libro() const;
+	list<char> * getIndexado() const;
+	bool getModificado() const;
+	uint32_t getOffset() const;
+	void setEspacioLibre(uint32_t espacioLibre);
+	void setId_Libro(uint32_t id_Libro);
+	void setIndexado(list<char> *indexado);
+	void setModificado(bool modificado);
+	void setOffset(uint32_t offset);
 
 	virtual ~DatoControlLibro();
 };
