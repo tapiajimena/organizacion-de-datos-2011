0717774f3e6d73ffa5bacdaf95b0b86b3af4fd45
smaraggi@gmail.com <smaraggi@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-15 02:49:03 +0000

    Subidos todos los parsers. Hay un parser por cada atributo (lista de palabras cuenta como uno) y todos heredan de la clase padre 'ParserDeAtributo'.

---
+++ TPDatos/src/ModuloDeInterfaz/ParserDePalabras.cpp
@@ -0,0 +1,45 @@
+/*
+ * ParserDePalabras.cpp
+ *
+ *  Created on: 12/04/2011
+ *      Author: santi
+ */
+
+#include "ParserDePalabras.h"
+
+ParserDePalabras::ParserDePalabras(std::string nombreArchivoStopWords) {
+
+	this->diccionarioStopWords = new Diccionario(nombreArchivoStopWords);
+
+	this->diccionarioStopWords->cargarDiccionario();
+}
+
+ParserDePalabras::~ParserDePalabras() {
+	delete this->diccionarioStopWords;
+}
+
+void ParserDePalabras::procesarLineaLibro(std::string linea, Libro* libro)
+{
+	//Definir si la cadena tiene el formato especial de alguno de los campos especiales...
+	std::vector<std::string> palabrasDeLinea = ServiceClass::obtenerListaPalabras(linea, SEPARADORES_DE_PALABRAS);	std::vector<std::string>::iterator it_PalabrasDeLinea;	for(it_PalabrasDeLinea = palabrasDeLinea.begin(); it_PalabrasDeLinea != palabrasDeLinea.end(); it_PalabrasDeLinea++)
+	{		std::string palabra = *it_PalabrasDeLinea;		//Trabajamos las palabras en minúsculas.		palabra = ServiceClass::toDowncase(palabra);		//EstructuraStopWords::iterator it_StopWords = this->listaStopWords.find(palabra);		bool esStopWord = this->diccionarioStopWords->existePalabra(palabra);
+		if (!esStopWord)		{			libro->agregarPalabraClave(palabra);		}	}}
+Libro* ParserDePalabras::parsear(DatoLibro* datoLibro)
+{
+
+	//Creamos un libro vacío. Se le carga sólo la editorial.
+   	Libro* libro = new Libro();
+
+	//bool tituloEncontrado = false;	//Trabajamos con una copia del dato	std::string cadenaLibro = datoLibro->getDato();	std::string renglonLibro;
+	bool finCadena = false;	size_t posicionFin = 0;	size_t posicionInicio = 0;	if (!cadenaLibro.empty())	{		while(posicionFin != string::npos) //&& !finCadena)		{			posicionFin = cadenaLibro.find(CARACTER_FIN_DE_LINEA, posicionInicio);			if (posicionFin != string::npos)			{				//subcadena desde posición inicio hasta (posiciónFin-1 -posicionInicio). Equivale a un renglón.				renglonLibro = cadenaLibro.substr(posicionInicio, (posicionFin - 1)-posicionInicio);			}			else			{
+				finCadena = true;
+				if(posicionInicio < cadenaLibro.size()-1)
+				{
+					renglonLibro = cadenaLibro.substr(posicionInicio, cadenaLibro.size()-1);
+				}
+				else				{
+					renglonLibro = "";
+				}			}			this->procesarLineaLibro(renglonLibro, libro);			//posición siguiente al último fin de línea; en caso de fin de cadena, no se usa.			posicionInicio = posicionFin+1;		}	}
+    return libro;
+
+}
