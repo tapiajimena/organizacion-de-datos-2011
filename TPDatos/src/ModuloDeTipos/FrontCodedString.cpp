26ae4ab092bc27c0df5f231d4bb27364fa0bcdbd
paulofer85@gmail.com <paulofer85@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-16 16:23:06 +0000

    nuevas clases para el FontCoding

---
+++ TPDatos/src/ModuloDeTipos/FrontCodedString.cpp
@@ -0,0 +1,22 @@
+/*
+ * FrontCoderString.cpp
+ *
+ *    Autor   	 : GRUPO 1 - Fernandez, Gallinal, Maraggi, Tapia
+ *    Catedra    : SERVETTO-FERRER-FERNANDEZ
+ *    Materia    : Organizacion de Datos (75.06) - FIUBA
+ *
+ *
+ *
+ *
+ */
+
+#include "FrontCodedString.h"
+
+FrontCodedString::FrontCodedString() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FrontCodedString::~FrontCodedString() {
+	// TODO Auto-generated destructor stub
+}
