4d95148170c0e1f02022855ca227e9c5b24566fc
jimena.soledad.tapia <jimena.soledad.tapia@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-20 02:10:07 +0000

    nuevo metodo con parametro auxiliar

--- TPDatos/Common/Utilitarios/Logger.cpp
+++ TPDatos/Common/Utilitarios/Logger.cpp
@@ -11,5 +11,13 @@ void Logger::log(string clase, string metodo, string mensaje) {
 			<< metodo << "]: " << mensaje << endl;
 }
 
+void Logger::log(string clase, string metodo, string mensaje, string aux) {
+
+	string nombreClase = ServiceClass::toUppercase(clase);
+
+	cout << ServiceClass::getFechaHora() << " - " << nombreClase << " ["
+			<< metodo << "]: " << mensaje << " " << aux << endl;
+}
+
 Logger::~Logger() {
 }
