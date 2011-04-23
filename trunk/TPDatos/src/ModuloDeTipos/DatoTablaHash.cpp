5aca2b177a1246eec065e8038771e2fd531b4a85
smaraggi@gmail.com <smaraggi@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-23 21:26:58 +0000

    Clases que usa el Hash

---
+++ trunk/TPDatos/src/ModuloDeTipos/DatoTablaHash.cpp
@@ -0,0 +1,17 @@
+/*
+ * DatoTablaHash.cpp
+ *
+ *  Created on: 22/04/2011
+ *      Author: santi
+ */
+
+#include "DatoTablaHash.h"
+
+DatoTablaHash::DatoTablaHash() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DatoTablaHash::~DatoTablaHash() {
+	// TODO Auto-generated destructor stub
+}
