00d1f9a230d5ad2b2d79f98ae22956e0a2a9c1ba
paulofer85@gmail.com <paulofer85@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-24 22:07:03 +0000

    +Parser

---
+++ trunk/TPDatos/src/ModuloParser/ParserDeAtributo.h
@@ -0,0 +1,35 @@
+/*
+ * ParserDeLibros.h
+ *
+ *    Autor		 : GRUPO 1 - Fernandez, Gallinal, Maraggi, Tapia
+ *    Catedra    : SERVETTO-FERRER-FERNANDEZ
+ *    Materia    : Organizacion de Datos (75.06) - FIUBA
+ *
+ *	Clase que ofrece el servicio de parseo de los libros. Sirve para cargar los registros de la
+ *	estructura libro desde un archivo de libro en formato .txt
+ */
+
+#ifndef PARSERDEATRIBUTO_H_
+#define PARSERDEATRIBUTO_H_
+
+#include "../ModuloEstructuras/Libro.h"
+#include "../ModuloDeTipos/DatoLibro.h"
+#include "../Common/Constantes.h"
+#include "../Common/Utilitarios/ServiceClass.h"
+#include "../ModuloEstructuras/Diccionario.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+
+class ParserDeAtributo {
+public:
+	//ParserDeAtributo();
+	//virtual ~ParserDeAtributo();
+
+	//Método abstracto que reimplementan los herederos.
+	//Devuelve un libro en el que solamente se carga el atributo que es propio del
+	//parser según su tipo específico (el resto de los atributos del libro quedan vacíos).
+	virtual Libro* parsear(DatoLibro* datoLibro) = 0;
+};
+
+#endif /* PARSERDEATRIBUTO_H_ */
