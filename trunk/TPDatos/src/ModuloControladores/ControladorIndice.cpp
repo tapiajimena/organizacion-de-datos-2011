7b225cca4dc1c4d43c4ddc82d3141a9fe772a2d9
paulofer85@gmail.com <paulofer85@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-24 06:26:30 +0000

    +ControladorIndice: crea nuevos indices y permite la indexasion de un dato a un arbol o hash. TODO hacer un test

---
+++ trunk/TPDatos/src/ModuloControladores/ControladorIndice.cpp
@@ -0,0 +1,107 @@
+/*
+ * ControladorIndice.cpp
+ *  
+ *	Autor	: GRUPO 1 - Fernandez, Gallinal, Maraggi, Tapia
+ *	Catedra	: SERVETTO-FERRER-FERNANDEZ
+ *	Materia	: Organizacion de Datos (75.06) - FIUBA
+ *
+ *
+ *
+ *
+ */
+
+#include "ControladorIndice.h"
+
+
+ControladorIndice::ControladorIndice()
+{
+	this->pathCarpeta = CARPETA_DEFAULT;
+}
+
+
+ControladorIndice::ControladorIndice(string pathCarpeta)
+{
+	this->pathCarpeta = pathCarpeta;
+}
+
+
+void ControladorIndice::nuevoIndiceAutor()
+{
+	this->indiceArbol = new ArbolBMas(pathCarpeta+ARCHIVO_INDICE_AUTOR, SIZE_BLOQUE);
+}
+
+
+void ControladorIndice::nuevoIndiceEditorial()
+{
+	this->indiceArbol = new ArbolBMas(pathCarpeta+ARCHIVO_INDICE_EDITORIAL, SIZE_BLOQUE);
+}
+
+
+void ControladorIndice::nuevoIndiceTitulo()
+{
+	this->indiceArbol = new ArbolBMas(pathCarpeta+ARCHIVO_INDICE_TITULO, SIZE_BLOQUE);
+}
+
+
+void ControladorIndice::nuevoIndicePalabras()
+{
+	this->indiceArbol = new ArbolBMas(pathCarpeta+ARCHIVO_INDICE_PALABRAS, SIZE_BLOQUE);
+}
+
+
+void ControladorIndice::indexarPorAutor(pair<Libro*,uint32_t> parLibroOffset)
+{
+	DatoElementoNodo* ele = new DatoElementoNodo();
+
+	ele->setClave(((Libro*)(parLibroOffset.first))->getTitulo());
+	ele->agregarLibro(parLibroOffset.second);
+
+	this->indiceArbol->insertar(ele);
+	delete(ele);
+}
+
+
+void ControladorIndice::indexarPorEditorial(pair<Libro*,uint32_t> parLibroOffset)
+{
+	DatoElementoNodo* ele = new DatoElementoNodo();
+
+	ele->setClave(((Libro*)(parLibroOffset.first))->getEditorial());
+	ele->agregarLibro(parLibroOffset.second);
+
+	this->indiceArbol->insertar(ele);
+	delete(ele);
+}
+
+
+void ControladorIndice::indexarPorTitulo(pair<Libro*,uint32_t> parLibroOffset)
+{
+	pair<string,uint32_t> registroHash(parLibroOffset.first->getTitulo(), parLibroOffset.second);
+	this->indiceHash->insertarClave(registroHash);
+}
+
+
+void ControladorIndice::indexarPorPalabras(pair<Libro*,uint32_t> parLibroOffset)
+{
+	string 	aux;
+	pair<string,uint32_t> 				registroHash;
+	EstructuraPalabrasClave::iterator 	it;
+
+	for(it =parLibroOffset.first->getPalabrasClave()->begin();it!=parLibroOffset.first->getPalabrasClave()->end();++it)
+	{
+		aux = ServiceClass::normalizarString((*it).first);
+		registroHash.first = aux;
+		registroHash.second= parLibroOffset.second;
+		this->indiceHash->insertarClave(registroHash);
+	}
+}
+
+
+ControladorIndice::~ControladorIndice()
+{
+	delete (indiceHash);
+	delete (indiceArbol);
+}
+
+
+
+
---
+++ trunk/TPDatos/src/ModuloControladores/ControladorIndice.h
@@ -0,0 +1,77 @@
+/*
+ * ControladorIndice.h
+ *  
+ *	Autor	: GRUPO 1 - Fernandez, Gallinal, Maraggi, Tapia
+ *	Catedra	: SERVETTO-FERRER-FERNANDEZ
+ *	Materia	: Organizacion de Datos (75.06) - FIUBA
+ *      
+ *  
+ *	Se encarga del control de los indices.
+ *  Los cuatro tipos de indices que puede crear son:
+ *  		-Autor (usa un arbol)
+ *  		-Editorial (usa un arbol)
+ *  		-Titulo (usa un hash)
+ *  		-Palabras (Usa  hash)
+ */
+
+#ifndef CONTROLADORINDICE_H_
+#define CONTROLADORINDICE_H_
+
+#include <stdint.h>
+
+#include "../../Common/Utilitarios/ServiceClass.h"
+#include "../ModuloDeArchivos/ManejadorArchivo.h"
+#include "../ModuloDeInterfaz/Libro.h"
+#include "../ModuloDeIndices/Hash/Hash.h"
+#include "../ModuloDeIndices/ArbolBMas/src/ArbolBMas.h"
+
+
+using namespace std;
+class ControladorIndice
+{
+private:
+	Hash* 		indiceHash;
+	ArbolBMas* 	indiceArbol;
+	string		pathCarpeta;
+
+
+public:
+	/*
+	 * Setea el path de trabajo al default
+	 */
+	ControladorIndice();
+
+	/*
+	 * Se setea el path de trabajo
+	 */
+	ControladorIndice(string pathCarpeta);
+
+	/*
+	 *Crean un nuevo indice del tipo autor,
+	 *editorial, titulo y palabras.
+	 */
+	void nuevoIndiceAutor();
+	void nuevoIndiceEditorial();
+	void nuevoIndiceTitulo();
+	void nuevoIndicePalabras();
+
+
+	/*
+	 * Indexan en un indice Arbol el autor o editorial
+	 *
+	 */
+	void indexarPorAutor(pair<Libro*,uint32_t> parLibroOffset);
+	void indexarPorEditorial(pair<Libro*,uint32_t> parLibroOffset);
+
+	/*
+	 * Indexan en un indice Hash el titulo o palabras.
+	 *
+	 */
+	void indexarPorTitulo(pair<Libro*,uint32_t> parLibroOffset);
+	void indexarPorPalabras(pair<Libro*,uint32_t> parLibroOffset);
+
+
+	virtual ~ControladorIndice();
+};
+
+#endif /* CONTROLADORINDICE_H_ */
