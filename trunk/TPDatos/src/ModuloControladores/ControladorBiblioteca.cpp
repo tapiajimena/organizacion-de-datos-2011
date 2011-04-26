efac1e09a3d1aa8e0940194aa8003f792f2fd8b9
jimena.soledad.tapia <jimena.soledad.tapia@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-26 01:36:52 +0000

    se termina la implementacion

--- trunk/TPDatos/src/ModuloControladores/ControladorBiblioteca.cpp
+++ trunk/TPDatos/src/ModuloControladores/ControladorBiblioteca.cpp
@@ -22,57 +22,51 @@ ControladorBiblioteca::ControladorBiblioteca(string pathBiblioteca,
 }
 
 bool ControladorBiblioteca::ingresarLibro(string pathLibro) {
+	/* Se actualiza la insercion en el archivo de control */
+	uint32_t nuevoOffset = arcControl->registrarLibro(
+			GetSizeArchivo(pathLibro), arcLibro->getSizeArchivo());
 
 	/* Se agrega el libro en el archivo de registros variables */
-	arcLibro->agregarLibro((char*) pathLibro.c_str());
+	arcLibro->agregarLibro((char*) pathLibro.c_str(), nuevoOffset);
 
-	/* Se actualiza la insercion en el archivo de control */
-	//uint32_t nuevoOffset = arcControl->registrarLibro(size,finArchivoLibros);
-
-	/* Tengo que preguntar donde escribo y despues pasarselo al registrar de
-	 * archivo de control. Y despues guardarlo en el de archivo variables. */
-
+	arcControl->actualizarArchivo();
 }
 
 bool ControladorBiblioteca::eliminarLibro(uint32_t offset) {
-	//TODO actualizar el estado del control del archivo
-	arcLibro->eliminar((uint32_t) offset);
+	DatoLibro datoLibro = recuperarLibro(offset);
+
+	/* Actualiza el estado del archivo de control */
+	arcControl->eliminarLibro(offset, datoLibro.getSize());
 }
 
 DatoLibro ControladorBiblioteca::recuperarLibro(uint32_t offset) {
 	return arcLibro->recuperarLibro(offset);
 }
 
-
-
-list<uint32_t> ControladorBiblioteca::recuperarLibrosNoIndexadosPor(char tipoIndice)
-{
+list<uint32_t> ControladorBiblioteca::recuperarLibrosNoIndexadosPor(
+		char tipoIndice) {
 	//TODO como se hace para obtener el offset a libro del controlBiblioteca
 	list<uint32_t> idLibros;
 	DatoControlLibro* dControlLibro;
-	map<uint32_t,DatoControlLibro*>::iterator it;
-	map<uint32_t,DatoControlLibro*>* librosControl = arcControl->getLibros();
-
+	map<uint32_t, DatoControlLibro*>::iterator it;
+	map<uint32_t, DatoControlLibro*>* librosControl = arcControl->getLibros();
 
 	map<uint32_t, DatoControlLibro*>::iterator it_aux;
 
-	if (!librosControl->empty())
-	{
-		while (it != librosControl->end())
-		{
+	if (!librosControl->empty()) {
+		while (it != librosControl->end()) {
 			dControlLibro = (*it).second;
 			if (dControlLibro->isIndexadoPor(tipoIndice))
 				idLibros.push_back((*it).first);
 		}
-	}
-	else
-		Logger::log("ControladorBiblioteca", "recuperarLibrosNoIndexadosPor","la lista de control esta vacia");
+	} else
+		Logger::log("ControladorBiblioteca", "recuperarLibrosNoIndexadosPor",
+				"la lista de control esta vacia");
 	return idLibros;
 }
 
-
 string ControladorBiblioteca::getPathControlBiblioteca() {
-	//return arcControl->getPathArchivoControlLibro();
+	return arcControl->getPathArchivoControlLibro();
 }
 
 string ControladorBiblioteca::getPathBiblioteca() {
