a320cec56f265fe3eefb68696432692389820be1
paulofer85@gmail.com <paulofer85@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-27 06:01:37 +0000

    +eliminar indices y otras yerbas

--- TPDatos/src/ModuloControladores/ControladorIndice.cpp
+++ TPDatos/src/ModuloControladores/ControladorIndice.cpp
@@ -49,27 +49,57 @@ void ControladorIndice::nuevoIndicePalabras()
 }
 
 
-void ControladorIndice::indexarPorAutor(pair<Libro*,uint32_t> parLibroOffset)
+void ControladorIndice::indexar(pair<Libro*,uint32_t> parLibroOffset, char tipoIndice)
+{
+	//si queda tiempo crear clases
+	if ((tipoIndice == INDICE_AUTOR)||(tipoIndice == INDICE_EDITORIAL))
+		indexarPorAutorOEditorial(parLibroOffset);
+	else if(tipoIndice == INDICE_TITULO)
+		indexarPorTitulo(parLibroOffset);
+	else if(tipoIndice == INDICE_PALABRAS)
+		indexarPorPalabras(parLibroOffset);
+	else
+		Logger::log("ControladorIndice","indexar","El tipo de indice no existe, verifique que sea A,E,T,P");
+}
+
+
+void ControladorIndice::eliminarIndexado(Libro* libroRemover)
+{
+	//se remueven los indices de autor del arbol
+	ArbolBMas* indiceAutorArbol = new ArbolBMas(pathCarpeta+ARCHIVO_INDICE_AUTOR, SIZE_BLOQUE);
+	//indiceAutorArbol->eliminar(libroRemover->getAutor());
+	delete(indiceAutorArbol);
+
+	//se remueven los indices de editorial del arbol
+	ArbolBMas* indiceEditorialArbol = new ArbolBMas(pathCarpeta+ARCHIVO_INDICE_EDITORIAL, SIZE_BLOQUE);
+	//indiceEditorialArbol->eliminar(libroRemover->getEditorial());
+	delete(indiceEditorialArbol);
+
+	//se remueven los indices de titulo del hash
+	Hash* indiceTituloHash= new Hash(pathCarpeta+ARCHIVO_INDICE_TITULO, pathCarpeta+ARCHIVO_CUBETAS);
+	//indiceTituloHash->eliminar(libroRemover->getAutor());
+	delete(indiceTituloHash);
+
+
+	/*
+	 //TODO hacer llegar la lista de palabras
+	//se remueven los indices de palabras el hash
+	Hash* indicePalabrasHash= new Hash(pathCarpeta+ARCHIVO_INDICE_PALABRAS, pathCarpeta+ARCHIVO_CUBETAS);
+	indicePalabrasHash->eliminar(ACA LA LISTA DE PALABRAS);
+	delete(indicePalabrasHash);
+	*/
+
+}
+
+
+void ControladorIndice::indexarPorAutorOEditorial(pair<Libro*,uint32_t> parLibroOffset)
 {
 	DatoElementoNodo* ele = new DatoElementoNodo();
-
 
 	ele->setClave(((Libro*)(parLibroOffset.first))->getAutor());
 	ele->agregarLibro(parLibroOffset.second);
 
 	//this->indiceArbol->insertar(ele);
-	delete(ele);
-}
-
-
-void ControladorIndice::indexarPorEditorial(pair<Libro*,uint32_t> parLibroOffset)
-{
-	DatoElementoNodo* ele = new DatoElementoNodo();
-
-	ele->setClave(((Libro*)(parLibroOffset.first))->getEditorial());
-	ele->agregarLibro(parLibroOffset.second);
-
-	this->indiceArbol->insertar(ele);
 	delete(ele);
 }
 
