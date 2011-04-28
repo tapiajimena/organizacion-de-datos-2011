a7283fcd3060077a153082443b415301fc4d48f6
paulofer85@gmail.com <paulofer85@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-04-28 05:45:48 +0000

    +Configuracion

---
+++ TPDatos/src/ModuloEstructuras/Configuracion.h
@@ -0,0 +1,62 @@
+/*
+ * Configuracion.h
+ *  
+ *	Autor	: GRUPO 1 - Fernandez, Gallinal, Maraggi, Tapia
+ *	Catedra	: SERVETTO-FERRER-FERNANDEZ
+ *	Materia	: Organizacion de Datos (75.06) - FIUBA
+ *      
+ *  
+ * Contiene la informacion de configuracion, path de trabajo y ruta al archivo de stopwords.
+ * Abre el archivo de configuracion lo interpreta y lo cierra.
+ *      
+ */
+
+#ifndef CONFIGURACION_H_
+#define CONFIGURACION_H_
+
+#include <iostream>
+#include <string.h>
+
+#include "../Common/Constantes.h"
+#include "../Common/Utilitarios/Logger.h"
+#include "../Common/Utilitarios/ServiceClass.h"
+#include "../ModuloEstructuras/Configuracion.h"
+#include "../ModuloDeArchivos/ManejadorArchivo.h"
+
+using namespace ManejadorArchivo;
+class Configuracion
+{
+private:
+	static 	Configuracion* pInstancia;
+	string	pathCarpetaTrabajo;
+	string	pathArchivoStopWords;
+
+
+public:
+	static Configuracion* GetInstancia(string pathConfiguracion);
+
+	/*
+	 * Getters y setters
+	 */
+	string getPathArchivoStopWords() const;
+    string getPathCarpetaTrabajo() const;
+    void setPathArchivoStopWords(string pathArchivoStopWords);
+    void setPathCarpetaTrabajo(string pathCarpetaTrabajo);
+
+private:
+
+    /*
+     * Obtiene el contenido del archivo de configuracion.
+     * Interpreta este archivo de la siguiente forma:
+     *Ej:
+     * 		#CARPETA_TRABAJO=/Indices
+	 *		#ARCHIVO_STOPWORDS=stopwordstest.txt
+	 *@param pathConfiguracion ruta al archivo de configuracion
+     */
+	Configuracion(string pathConfiguracion);
+	virtual ~Configuracion();
+
+
+};
+
+#endif /* CONFIGURACION_H_ */
