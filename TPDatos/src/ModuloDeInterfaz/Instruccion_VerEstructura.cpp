19089e29d0fcb74f11b4a472aeb95dc3519e13aa
jimena.soledad.tapia@gmail.com <jimena.soledad.tapia@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-05-02 02:16:59 +0000

    faltan muchas cosas ...  ver mañana 2/5 - IMPORTANTE

--- TPDatos/src/ModuloDeInterfaz/Instruccion_VerEstructura.cpp
+++ TPDatos/src/ModuloDeInterfaz/Instruccion_VerEstructura.cpp
@@ -16,7 +16,36 @@ Instruccion_VerEstructura::Instruccion_VerEstructura(char id,
 	this->nombreArchivo = nombreArchivo;
 }
 
+void Instruccion_VerEstructura::crearArchivos() {
+	if ((this->tipoEstructura == INDICE_AUTOR || this->tipoEstructura
+			== INDICE_EDITORIAL)) {
+		string archivoEstructura = this->nombreArchivo + ARCHIVO_INDICE_SUFIX;
+	} else {
+		string archivoEstructura = this->nombreArchivo + ARCHIVO_TABLA_SUFIX;
+	}
+	Crear(archivoEstructura, this->archivoEstructuras, true);
+
+	string archivoEspaciosLibres = this->nombreArchivo
+			+ ARCHIVO_ESPACIOSLIBRES_SUFIX;
+	Crear(archivoEspaciosLibres, this->archivoEspaciosLibres, true);
+
+	string archivoBloques = this->nombreArchivo + ARCHIVO_BLOQUES_SUFIX;
+	Crear(archivoBloques, this->archivoBloques, true);
+}
+
 void Instruccion_VerEstructura::ejecutar() {
+	Configuracion* conf = Configuracion::GetInstancia();
+
+	Indexador* indexador = new Indexador();
+
+	//genero el indice correspondiente
+	indexador->indexar(ServiceClass::toUppercase(this->tipoEstructura));
+
+	//armo los archivos.
+	crearArchivos();
+
+	//cargar archivos
+
 
 	Logger::log("Instruccion_VerEstructura", "ejecutar",
 			"Se muestra la estructura.");
