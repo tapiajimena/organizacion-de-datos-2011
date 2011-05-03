1bfc15ac6493c493d182cae412b724ef2089f52a
paulofer85@gmail.com <paulofer85@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-05-03 02:43:14 +0000

    +reporte control de biblioteca

--- trunk/TPDatos/src/ModuloDeInterfaz/Instruccion_VerEstructura.cpp
+++ trunk/TPDatos/src/ModuloDeInterfaz/Instruccion_VerEstructura.cpp
@@ -16,8 +16,24 @@ Instruccion_VerEstructura::Instruccion_VerEstructura(char id,
 	this->nombreArchivo = nombreArchivo;
 }
 
+
+void Instruccion_VerEstructura::crearReporteArchivoControl(string nombreArchivo)
+{
+	Configuracion* conf = Configuracion::GetInstancia();
+
+	CrearDirectorios(conf->getPathCarpetaReportes());
+	CopiarArchivo(conf->getPathCarpetaTrabajo() + ARCHIVO_CONTROL_BIBLIOTECA,
+						conf->getPathCarpetaReportes());
+	RenombrarArchivo( conf->getPathCarpetaReportes() + ARCHIVO_CONTROL_BIBLIOTECA,
+									conf->getPathCarpetaReportes() + nombreArchivo +
+									ARCHIVO_ESPACIOSLIBRES_SUFIX + EXTENSION_ARCHIVO_REPORTE);
+}
+
 void Instruccion_VerEstructura::crearArchivos() {
 	string archivoEstructura;
+
+	crearReporteArchivoControl(this->nombreArchivo);
+
 	if ((this->tipoEstructura == INDICE_AUTOR || this->tipoEstructura
 			== INDICE_EDITORIAL)) {
 		archivoEstructura = this->nombreArchivo + ARCHIVO_INDICE_SUFIX;
