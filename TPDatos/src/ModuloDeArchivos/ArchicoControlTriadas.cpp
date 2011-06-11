15b86e07844cc670c5a8549452b1da856ba4b982
jimena.soledad.tapia@gmail.com <jimena.soledad.tapia@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-06-11 22:38:26 +0000

    archivo control triadas

---
+++ TPDatos/src/ModuloDeArchivos/ArchicoControlTriadas.cpp
@@ -0,0 +1,19 @@
+/*
+ * ArchicoControlTriadas.cpp
+ *
+ *	Autor	: GRUPO 1 - Fernandez, Gallinal, Maraggi, Tapia
+ *	Catedra	: SERVETTO-FERRER-FERNANDEZ
+ *	Materia	: Organizacion de Datos (75.06) - FIUBA
+ *
+ */
+
+#include "ArchicoControlTriadas.h"
+
+ArchicoControlTriadas::ArchicoControlTriadas() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ArchicoControlTriadas::~ArchicoControlTriadas() {
+	// TODO Auto-generated destructor stub
+}
