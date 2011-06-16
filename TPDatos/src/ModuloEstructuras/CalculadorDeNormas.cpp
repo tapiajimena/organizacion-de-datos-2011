8c25ece63a3ef3ef93a4b1d5d5d385e75795c0ac
smaraggi@gmail.com <smaraggi@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-06-16 05:48:19 +0000

    Avances varios... (falta).

--- TPDatos/src/ModuloEstructuras/CalculadorDeNormas.cpp
+++ TPDatos/src/ModuloEstructuras/CalculadorDeNormas.cpp
@@ -7,34 +7,143 @@
 
 #include "CalculadorDeNormas.h"
 
-CalculadorDeNormas::CalculadorDeNormas() {
+CalculadorDeNormas::CalculadorDeNormas(ControladorIndice* controladorIndice, ControladorTriadas* controladorDeTriadas)
+{
 	Configuracion* conf = Configuracion::GetInstancia();
 	this->pathCarpeta = conf->getPathCarpetaTrabajo();
-
-	//TODO obtener...
-	//...archivo de triadas
-	//...puntero al arbol de terminos para acceder a las triadas.
+	this->controladorDeTriadas = controladorDeTriadas;
+	this->controladorIndice = controladorIndice;
 }
 
-CalculadorDeNormas::~CalculadorDeNormas() {
+CalculadorDeNormas::~CalculadorDeNormas()
+{
+	//no destruye las estructuras que usa.
+}
+
+std::vector<DatoTriada> CalculadorDeNormas::levantarTriadasDeTermino(uint32_t idTermino)
+{
+	std::vector<DatoTriada> ocurrenciasDeTermino;
+
+	//TODO...
+
+	return ocurrenciasDeTermino;
 }
 
 int CalculadorDeNormas::calcularDocumentosQueContienenTermino(uint32_t idTermino)
 {
+	int documentosQueContienenTermino = 0;
 
+	std::vector<DatoTriada> ocurrenciasDeTermino = this->levantarTriadasDeTermino(idTermino);
+
+	//En este vector se guardan los libros en que aparece el término. La idea es no contar
+	//dos ocurrencias si son del mismo documento, ya que interesa la cantidad de DOCUMENTOS
+	//que contienen alguna vez al término.
+	std::vector<uint32_t> documentosYaVisitados;
+
+	std::vector<DatoTriada>::iterator it_triadas;
+
+	std::vector<uint32_t>::iterator it_documentos;
+
+	bool documentoYaVisitado;
+
+	for(it_triadas = ocurrenciasDeTermino.begin();
+		it_triadas != ocurrenciasDeTermino.end();
+		it_triadas++)
+	{
+		documentoYaVisitado = false;
+
+		//Si el documento de la ocurrencia no fue ya visitado, se cuenta la ocurrencia y
+		//se agrega el documento como visitado, si no, se ignora la ocurrencia.
+		for(it_documentos = documentosYaVisitados.begin();
+			it_documentos != documentosYaVisitados.end() && !documentoYaVisitado;
+			it_documentos++)
+		{
+			if(it_triadas->getIdLibro() == *it_documentos)
+			{
+				documentoYaVisitado = true;
+			}
+		}
+
+		if(!documentoYaVisitado)
+		{
+			documentosQueContienenTermino++;
+			documentosYaVisitados.push_back(it_triadas->getIdLibro());
+		}
+	}
+
+	return documentosQueContienenTermino;
+}
+
+float CalculadorDeNormas::calcularPesoGlobalDeTermino(uint32_t idTermino)
+{
+	float pesoGlobal = 0.0;
+
+	int frecuenciaGlobalDeTermino = this->calcularDocumentosQueContienenTermino(idTermino);
+
+	if(frecuenciaGlobalDeTermino != 0)
+	{
+		pesoGlobal = log10( (float)(this->cantidadTotalDeDocumentos / frecuenciaGlobalDeTermino));
+	}
+
+	return pesoGlobal;
 }
 
 int CalculadorDeNormas::calcularAparicionesDeTerminoEnDocumento(uint32_t idDocumento, uint32_t idTermino)
 {
+	int ocurrenciasDeTerminoEnDocumento = 0;
+
+	std::vector<DatoTriada> ocurrenciasDeTermino = this->levantarTriadasDeTermino(idTermino);
+
+	std::vector<DatoTriada>::iterator it_ocurrencias;
+
+	for(it_ocurrencias = ocurrenciasDeTermino.begin();
+		it_ocurrencias != ocurrenciasDeTermino.end();
+		it_ocurrencias++)
+	{
+		if(it_ocurrencias->getIdLibro() == idDocumento)
+		{
+			ocurrenciasDeTerminoEnDocumento++;
+		}
+	}
+
+	return ocurrenciasDeTerminoEnDocumento;
+}
+
+float CalculadorDeNormas::calcularPesoLocalDeTermino(uint32_t idDocumento, uint32_t idTermino)
+{
+	int frecuenciaLocal = this->calcularAparicionesDeTerminoEnDocumento(idDocumento, idTermino);
+
+	float pesoGlobal; //TODO ...estructura???
+
+	float normaDocumento; //TODO ...???
+
+	return frecuenciaLocal*pesoGlobal/normaDocumento;
+}
+
+float CalculadorDeNormas::calcularNormaDeDocumento(uint32_t idDocumento)
+{
 
 }
 
-int CalculadorDeNormas::calcularNormaDeDocumeto(uint32_t idDocumento)
+float CalculadorDeNormas::calcularProductoInterno(uint32_t idDocumento, std::string consulta)
+{
+
+}
+
+float CalculadorDeNormas::obtenerNormaDocumento(uint32_t idDocumento)
+{
+
+}
+
+float CalculadorDeNormas::obtenerNormaConsulta(std::string consulta)
 {
 
 }
 
 float CalculadorDeNormas::calcularSimilitudConsultaDocumento(uint32_t idDocumento, std::vector<std::string> consulta)
 {
+	//float productoInterno = this->calcularProductoInterno(idDocumento, consulta);
+	//float productoDeNormas = this->obtenerNormaDocumento(idDocumento) * this->obtenerNormaConsulta(consulta);
 
+	//return productoInterno / productoDeNormas;
 }
--- TPDatos/src/ModuloEstructuras/CalculadorDeNormas.h
+++ TPDatos/src/ModuloEstructuras/CalculadorDeNormas.h
@@ -13,32 +13,60 @@
 #include "../../Common/Utilitarios/ServiceClass.h"
 #include "../ModuloDeArchivos/ManejadorArchivo.h"
 #include "../ModuloEstructuras/Configuracion.h"
-//#include "../ModuloDeIndices/Hash/Hash.h"
-//arbol B+ (indice de triadas).
-//algo de las triadas... (Jime)
-
+#include "../ModuloControladores/ControladorTriadas.h"
+#include "../ModuloControladores/ControladorIndice.h"
+#include "../ModuloDeTipos/DatoTriada.h"
+#include "../ModuloDeIndices/Hash/Hash.h"
 
 class CalculadorDeNormas {
 private:
+	//ATRIBUTOS----------------------------------------------------------
+
 	//path de trabajo
 	string		pathCarpeta;
 
+	ControladorTriadas* controladorDeTriadas;
+
+	ControladorIndice* controladorIndice;
+
 	int cantidadTotalDeDocumentos;
 
+	//METODOS PRIVADOS---------------------------------------------------
+
+	std::vector<DatoTriada> levantarTriadasDeTermino(uint32_t idTermino);
+
 	//Devuelve la cantidad total de documentos que contienen al menos una vez un término
+	//También llamado "FRECUENCIA GLOBAL DE TERMINO" (cantidad de documentos en que aparece)
 	int calcularDocumentosQueContienenTermino(uint32_t idTermino);
 
+	//PESO GLOBAL DE TERMINO
+	float calcularPesoGlobalDeTermino(uint32_t idTermino);
+
+	//Devuelve la cantidad total de apariciones de un documento en un término.
+	//También llamado "FRECUENCIA LOCAL DE TERMINO" de un documento.
 	int calcularAparicionesDeTerminoEnDocumento(uint32_t idDocumento, uint32_t idTermino);
 
-	int calcularNormaDeDocumeto(uint32_t idDocumento);
+	//PESO LOCAL DE TERMINO
+	//Se usa para las consultas de similitud, con las normas ya calculadas y guardadas en un archivo.
+	float calcularPesoLocalDeTermino(uint32_t idDocumento, uint32_t idTermino);
+
+	float calcularProductoInterno(uint32_t idDocumento, std::string consulta);
+
+	float obtenerNormaDocumento(uint32_t idDocumento);
+
+	float obtenerNormaConsulta(std::string consulta);
+
+public:
+	CalculadorDeNormas(ControladorIndice* controladorIndice, ControladorTriadas* controladorDeTriadas);
+
+	virtual ~CalculadorDeNormas();
 
 	//Devuelve el coseno del ángulo vectorial entre dos documentos o...
 	//... un documento y una consulta (la consulta se toma como documento de pocos terminos)
 	float calcularSimilitudConsultaDocumento(uint32_t idDocumento, std::vector<std::string> consulta);
 
-public:
-	CalculadorDeNormas();
-	virtual ~CalculadorDeNormas();
+	//Calcula la norma de un documento
+	float calcularNormaDeDocumento(uint32_t idDocumento);
 };
 
 #endif /* CALCULADORDENORMAS_H_ */
