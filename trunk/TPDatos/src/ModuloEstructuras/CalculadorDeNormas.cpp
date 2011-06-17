7722193cea7ff67c106b72023a47dcaf9e4fab61
smaraggi@gmail.com <smaraggi@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-06-17 05:34:42 +0000

    Implementaciones varias.

--- trunk/TPDatos/src/ModuloEstructuras/CalculadorDeNormas.cpp
+++ trunk/TPDatos/src/ModuloEstructuras/CalculadorDeNormas.cpp
@@ -11,13 +11,42 @@ CalculadorDeNormas::CalculadorDeNormas(ControladorIndice* controladorIndice, Con
 {
 	Configuracion* conf = Configuracion::GetInstancia();
 	this->pathCarpeta = conf->getPathCarpetaTrabajo();
+
+	std::string nombreArchivoTablaPesos = this->pathCarpeta + "/TablaIndicePesos.dat";
+	std::string nombreArchivoCubetasPesos = this->pathCarpeta + "/CubetasIndicePesos.dat";
+
+	std::string nombreArchivoTablaNormas = this->pathCarpeta + "/TablaIndiceNormas.dat";
+	std::string nombreArchivoCubetasNormas = this->pathCarpeta + "/CubetasIndiceNormas.dat";
+
+	//Creamos y cerramos los archivos, por si antes fueron creados. Necesitamos empezar de cero, y el Hash...
+	//...está hecho para persistir y recuperar la información que guarda.
+	fstream arcTemporal;
+	ManejadorArchivo::Crear(nombreArchivoTablaPesos.c_str(), arcTemporal, true);
+	ManejadorArchivo::Cerrar(arcTemporal);
+	ManejadorArchivo::Crear(nombreArchivoCubetasPesos.c_str(), arcTemporal, true);
+	ManejadorArchivo::Cerrar(arcTemporal);
+	ManejadorArchivo::Crear(nombreArchivoTablaNormas.c_str(), arcTemporal, true);
+	ManejadorArchivo::Cerrar(arcTemporal);
+	ManejadorArchivo::Crear(nombreArchivoCubetasNormas.c_str(), arcTemporal, true);
+	ManejadorArchivo::Cerrar(arcTemporal);
+
+
+	this->indicePesosGlobales = new Hash(nombreArchivoTablaPesos, nombreArchivoCubetasPesos);
+
+	this->indiceNormasDocumentos = new Hash(nombreArchivoTablaNormas, nombreArchivoCubetasNormas);
+
+	//this->cantidadTotalDeDocumentos = ???; TODO
 	this->controladorDeTriadas = controladorDeTriadas;
 	this->controladorIndice = controladorIndice;
+
+	//hash para pesos globales... (crear vacio, pisar archivos viejos).
 }
 
 CalculadorDeNormas::~CalculadorDeNormas()
 {
-	//no destruye las estructuras que usa.
+	//Los índices quedan en disco por si se quieren ver de alguna manera...
+	delete this->indicePesosGlobales;
+	delete this->indiceNormasDocumentos;
 }
 
 std::vector<DatoTriada> CalculadorDeNormas::levantarTriadasDeTermino(uint32_t idTermino)
@@ -27,6 +56,11 @@ std::vector<DatoTriada> CalculadorDeNormas::levantarTriadasDeTermino(uint32_t id
 	//TODO...
 
 	return ocurrenciasDeTermino;
+}
+
+void CalculadorDeNormas::generarIndiceDePesosGlobalesDeTerminos()
+{
+	//iterar sobre los terminos... ver archivoTerminos.
 }
 
 int CalculadorDeNormas::calcularDocumentosQueContienenTermino(uint32_t idTermino)
@@ -92,6 +126,8 @@ int CalculadorDeNormas::calcularAparicionesDeTerminoEnDocumento(uint32_t idDocum
 {
 	int ocurrenciasDeTerminoEnDocumento = 0;
 
+	//Es más rápido pedir las ocurrencias del término que levantar todas las tríadas del libro.
+	//(salvo que se carguen millones de libros y que haya más ocurrencias del término que palabras en el libro)
 	std::vector<DatoTriada> ocurrenciasDeTermino = this->levantarTriadasDeTermino(idTermino);
 
 	std::vector<DatoTriada>::iterator it_ocurrencias;
@@ -109,33 +145,128 @@ int CalculadorDeNormas::calcularAparicionesDeTerminoEnDocumento(uint32_t idDocum
 	return ocurrenciasDeTerminoEnDocumento;
 }
 
+float CalculadorDeNormas::obtenerPesoGlobalDeIndice(uint32_t idTermino)
+{
+	float pesoGlobalDeTermino = 0.0;
+
+	unsigned int id = (unsigned int)idTermino;
+	std::string claveIndice = ServiceClass::obtenerString(id);
+
+	std::vector<uint32_t> resultadoConsulta;
+	resultadoConsulta = this->indicePesosGlobales->buscarPalabraEnHash(claveIndice);
+
+	if( resultadoConsulta.size() == 0)
+	{
+		//0.0
+	}
+	else
+	{
+		if(resultadoConsulta.size() == 1)
+		{
+			uint32_t enteroLeido = resultadoConsulta.at(0);
+			pesoGlobalDeTermino = ServiceClass::uint32tToFloat( enteroLeido );
+		}
+		else
+		{
+			std::cout<<"ERROR: multiples pesos globales para el termino"<<idTermino;
+			//throw??
+		}
+	}
+
+	return pesoGlobalDeTermino;
+}
+
+float CalculadorDeNormas::obtenerNormaDocumentoDeIndice(uint32_t idDocumento)
+{
+	float normaDocumento = 0.0;
+
+	unsigned int id = (unsigned int)idDocumento;
+	std::string claveIndice = ServiceClass::obtenerString(id);
+
+	std::vector<uint32_t> resultadoConsulta;
+	resultadoConsulta = this->indiceNormasDocumentos->buscarPalabraEnHash(claveIndice);
+
+	if( resultadoConsulta.size() == 0)
+	{
+		//0.0
+	}
+	else
+	{
+		if(resultadoConsulta.size() == 1)
+		{
+			uint32_t enteroLeido = resultadoConsulta.at(0);
+			normaDocumento = ServiceClass::uint32tToFloat( enteroLeido );
+		}
+		else
+		{
+			std::cout<<"ERROR: multiples normas para el documento"<<normaDocumento;
+			//throw??
+		}
+	}
+
+	return normaDocumento;
+}
+
 float CalculadorDeNormas::calcularPesoLocalDeTermino(uint32_t idDocumento, uint32_t idTermino)
 {
 	int frecuenciaLocal = this->calcularAparicionesDeTerminoEnDocumento(idDocumento, idTermino);
 
-	float pesoGlobal; //TODO ...estructura???
+	float pesoGlobal = this->obtenerPesoGlobalDeIndice(idTermino);
 
-	float normaDocumento; //TODO ...???
+	float normaDocumento = this->obtenerNormaDocumentoDeIndice(idDocumento);
 
 	return frecuenciaLocal*pesoGlobal/normaDocumento;
 }
 
 float CalculadorDeNormas::calcularNormaDeDocumento(uint32_t idDocumento)
 {
+	//Ya tienen que estar cargados los pesos globales de términos, se consulta en vez de recalcular.
+
+	//Este vector guarda todos los términos del documento, sin importar la cantidad de ocurrencias de los mismos.
+	std::vector<uint32_t> listaIdTerminosDeDocumento; //TODO cargar listaIdTerminosDeDocumento...
+
+	//Anotamos los términos ya procesados, para procesarlos una sola vez
+	std::vector<uint32_t> terminosYaProcesados;
+
+	std::vector<uint32_t>::iterator it_terminos;
+
+	std::vector<uint32_t>::iterator it_terminosYaProcesados;
+
+	float sumatoriaParcial = 0.0;
+
+	for(it_terminos = listaIdTerminosDeDocumento.begin(); it_terminos != listaIdTerminosDeDocumento.end(); it_terminos++)
+	{
+		bool terminoYaProcesado = false;
+
+		for( it_terminosYaProcesados = terminosYaProcesados.begin();
+			 it_terminosYaProcesados != terminosYaProcesados.end() && !terminoYaProcesado;
+			 it_terminosYaProcesados++)
+		{
+			if( *it_terminosYaProcesados == *it_terminos)
+			{
+				terminoYaProcesado = true;
+			}
+		}
+
+		if (!terminoYaProcesado)
+		{
+			float pesoGlobalTermino = this->obtenerPesoGlobalDeIndice(*it_terminos);
+
+			int frecuenciaLocalDeTermino = this->calcularAparicionesDeTerminoEnDocumento(idDocumento, *it_terminos);
+
+			sumatoriaParcial = sumatoriaParcial +  frecuenciaLocalDeTermino * pow(pesoGlobalTermino, 2);
+		}
+	}
+
+	return sqrt(sumatoriaParcial);
+}
+
+float CalculadorDeNormas::calcularNormaConsulta(std::string consulta)
+{
 
 }
 
 float CalculadorDeNormas::calcularProductoInterno(uint32_t idDocumento, std::string consulta)
-{
-
-}
-
-float CalculadorDeNormas::obtenerNormaDocumento(uint32_t idDocumento)
-{
-
-}
-
-float CalculadorDeNormas::obtenerNormaConsulta(std::string consulta)
 {
 
 }
--- trunk/TPDatos/src/ModuloEstructuras/CalculadorDeNormas.h
+++ trunk/TPDatos/src/ModuloEstructuras/CalculadorDeNormas.h
@@ -9,6 +9,7 @@
 #define CALCULADORDENORMAS_H_
 
 #include <stdint.h>
+#include <string>
 #include <math.h>
 #include "../../Common/Utilitarios/ServiceClass.h"
 #include "../ModuloDeArchivos/ManejadorArchivo.h"
@@ -29,10 +30,15 @@ private:
 
 	ControladorIndice* controladorIndice;
 
+	Hash* indicePesosGlobales;
+
+	Hash* indiceNormasDocumentos;
+
 	int cantidadTotalDeDocumentos;
 
 	//METODOS PRIVADOS---------------------------------------------------
 
+	//Accede al índice de tríadas y levanta todas las ocurrencias de un término
 	std::vector<DatoTriada> levantarTriadasDeTermino(uint32_t idTermino);
 
 	//Devuelve la cantidad total de documentos que contienen al menos una vez un término
@@ -46,15 +52,25 @@ private:
 	//También llamado "FRECUENCIA LOCAL DE TERMINO" de un documento.
 	int calcularAparicionesDeTerminoEnDocumento(uint32_t idDocumento, uint32_t idTermino);
 
+	//Consulta el índice de pesos globales y devuelve el del término. Si no está registrado, devuelve 0
+	float obtenerPesoGlobalDeIndice(uint32_t idTermino);
+
+	//Consulta el índice de normas y devuelve la del documento. Si no está registrado, devuelve 0
+	float obtenerNormaDocumentoDeIndice(uint32_t idDocumento);
+
 	//PESO LOCAL DE TERMINO
 	//Se usa para las consultas de similitud, con las normas ya calculadas y guardadas en un archivo.
 	float calcularPesoLocalDeTermino(uint32_t idDocumento, uint32_t idTermino);
 
 	float calcularProductoInterno(uint32_t idDocumento, std::string consulta);
 
-	float obtenerNormaDocumento(uint32_t idDocumento);
+	//Genera un índice de tipo Hash con todos los pesos globales de los términos.
+	void generarIndiceDePesosGlobalesDeTerminos();
 
-	float obtenerNormaConsulta(std::string consulta);
+	//Calcula la norma de un documento
+	float calcularNormaDeDocumento(uint32_t idDocumento);
+
+	float calcularNormaConsulta(std::string consulta);
 
 public:
 	CalculadorDeNormas(ControladorIndice* controladorIndice, ControladorTriadas* controladorDeTriadas);
@@ -65,8 +81,11 @@ public:
 	//... un documento y una consulta (la consulta se toma como documento de pocos terminos)
 	float calcularSimilitudConsultaDocumento(uint32_t idDocumento, std::vector<std::string> consulta);
 
-	//Calcula la norma de un documento
-	float calcularNormaDeDocumento(uint32_t idDocumento);
+	void generarArchivoDeNormasDeDocumentos();
+
+	//Busca en el archivo de normas la norma del documento por id. Se debe haber generado el archivo de
+	//normas en una corrida anterior. Si el archivo de normas no existe o el idDocumento no está en él, devuelve cero.
+	float obtenerNormaDocumentoIndexada();
 };
 
 #endif /* CALCULADORDENORMAS_H_ */
