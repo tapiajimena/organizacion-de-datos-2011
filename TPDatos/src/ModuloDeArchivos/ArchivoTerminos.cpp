017f8e7d93020c3a4de13b53020af310a32576ce
smaraggi@gmail.com <smaraggi@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-06-18 05:01:53 +0000

    Se agrego otro metodo que devuelve los pares <termino, idTermino>.

--- TPDatos/src/ModuloDeArchivos/ArchivoTerminos.cpp
+++ TPDatos/src/ModuloDeArchivos/ArchivoTerminos.cpp
@@ -171,3 +171,54 @@ std::vector<std::string> ArchivoTerminos::obtenerListaDeTerminos()
 	this->archivoTerminos.seekg(offsetPrevio, ios::beg);
 	return listaTerminos;
 }
+
+std::vector<std::pair<std::string, uint32_t> > ArchivoTerminos::obtenerTerminos()
+{
+	//Para dejar el archivo en la misma posición, tras terminar el método.
+	uint32_t offsetPrevio = this->archivoTerminos.tellg();
+
+	uint32_t PRR = 0; //puntero local
+
+	std::vector<std::pair<std::string, uint32_t> > listaTerminos;
+
+	uint32_t tamanioArchivoTerminos = ManejadorArchivo::GetSizeArchivo(this->archivoTerminos);
+
+	ManejadorArchivo::PosicionarPunteroLecturaEn(this->archivoTerminos, 0);
+
+	std::string palabra = "";
+	std::string letra = "";
+	char letraChar;
+	uint32_t offsetTermino = 0;
+
+	std::pair<std::string, uint32_t> elementoTermino;
+
+	while( PRR < tamanioArchivoTerminos)
+	{
+		//este metodo de la clase leerCaracteres no anda bien, o se pisa, que se yo que corno le pasa
+		//letraChar = ManejadorArchivo::LeerCaracteres(this->archivoTerminos, 1).at(0); //aca lee basura... ??
+
+		this->archivoTerminos.read(&letraChar, 1);
+
+		if(letraChar != FIN_DE_TERMINO)
+		{
+			letra = &letraChar;
+			palabra.append(letra);
+		}
+		else
+		{
+			if(palabra.size() > 0)
+			{
+				elementoTermino.first = palabra;
+				elementoTermino.second = offsetTermino;
+				listaTerminos.push_back(elementoTermino);
+			}
+			palabra = "";
+			offsetTermino = PRR + 1;
+		}
+
+		PRR = this->archivoTerminos.tellg();
+	}
+
+	this->archivoTerminos.seekg(offsetPrevio, ios::beg);
+	return listaTerminos;
+}
