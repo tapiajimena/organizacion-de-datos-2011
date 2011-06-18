a7f47d6222deaf64af97f5303d2392e6a09371cb
paulofer85@gmail.com <paulofer85@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-06-18 16:28:31 +0000

    +Instrucciones para buscar frases

---
+++ trunk/TPDatos/src/ModuloDeInterfaz/Instruccion_ConsultarTerminosProximos.cpp
@@ -0,0 +1,39 @@
+/*
+ * Instruccion_ConsultarTerminosProximos.cpp
+ *
+ *	Autor	: GRUPO 1 - Fernandez, Gallinal, Maraggi, Tapia
+ *	Catedra	: SERVETTO-FERRER-FERNANDEZ
+ *	Materia	: Organizacion de Datos (75.06) - FIUBA
+ *
+ */
+
+#include "Instruccion_ConsultarTerminosProximos.h"
+
+
+Instruccion_ConsultarTerminosProximos::Instruccion_ConsultarTerminosProximos(char id, string consulta):Instruccion(id)
+{
+	this->titulo = consulta;
+}
+
+
+void Instruccion_ConsultarTerminosProximos::ejecutar()
+{
+	Configuracion* conf = Configuracion::GetInstancia();
+	ConsultaIndice* consulta = new ConsultaIndice(conf->getPathCarpetaTrabajo());
+
+	Logger::log("Instruccion_ConsultarTerminosProximos", "ejecutar",
+			"Se ejecuta la consulta.");
+
+	cout<<"Se devuelven los datos consultados de titulo "<<endl;
+	consulta->consultarPalabras(titulo);
+
+	Logger::log("Instruccion_ConsultarTerminosProximos", "ejecutar",
+			"Se devuelve la consulta.");
+
+	delete(consulta);
+
+}
+
+Instruccion_ConsultarTerminosProximos::~Instruccion_ConsultarTerminosProximos() {
+	// TODO Auto-generated destructor stub
+}
