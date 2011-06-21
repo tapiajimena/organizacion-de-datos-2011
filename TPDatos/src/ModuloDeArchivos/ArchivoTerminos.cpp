19997288cacc8e58ca4b64763fc56d4f689e79d4
smaraggi@gmail.com <smaraggi@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-06-21 03:45:16 +0000

    Anda ArchivoTerminos bien, sin leaks ni errores en el Valgrind.

--- TPDatos/src/ModuloDeArchivos/ArchivoTerminos.cpp
+++ TPDatos/src/ModuloDeArchivos/ArchivoTerminos.cpp
@@ -9,18 +9,9 @@
 
 #include "ArchivoTerminos.h"
 
-
-ArchivoTerminos::ArchivoTerminos(string nombreArchivo)
+ArchivoTerminos::ArchivoTerminos(std::string nombreArchivo)
 {
-	char* cstrPath = new char[nombreArchivo.size() + 1];
-	strcpy(cstrPath, nombreArchivo.c_str());
-
-	if (ManejadorArchivo::Existe(cstrPath, this->archivoTerminos)) {
-		ManejadorArchivo::Abrir(cstrPath, this->archivoTerminos, true);
-	} else {
-		ManejadorArchivo::CrearSiNoExiste(cstrPath, this->archivoTerminos);
-	}
-	delete[] cstrPath;
+	ManejadorArchivo::CrearSiNoExiste( nombreArchivo.c_str(), archivoTerminos);
 }
 
 ArchivoTerminos::~ArchivoTerminos()
@@ -28,137 +19,83 @@ ArchivoTerminos::~ArchivoTerminos()
 	ManejadorArchivo::Cerrar(archivoTerminos);
 }
 
-uint32_t ArchivoTerminos::ingresarTermino(string palabra)
+uint32_t ArchivoTerminos::ingresarTermino(std::string palabra)
 {
+	//iostream* ios;
 	stringstream ss;
 
-	uint32_t offsetDeTermino = (uint32_t) ManejadorArchivo::GetSizeArchivo( this->archivoTerminos);
-	cout << "ingreso " << palabra << "en: " << offsetDeTermino << endl;
+	unsigned int tamanioDato = palabra.length();
+	uint32_t offsetDeTermino = ManejadorArchivo::GetSizeArchivo( this->archivoTerminos);
 
-	ss << palabra;
+	//El Offset en archivo del tï¿½rmino es su ID...
+	uint32_t idTermino = offsetDeTermino;
+
+	ss.write(palabra.c_str(), tamanioDato);
+
+	//Agregamos el caracter de fin de termino
 	ss << FIN_DE_TERMINO;
 
-	string aux = ss.str();
-	aux = aux.substr(0, aux.length() - 1);
+	ManejadorArchivo::Escribir(&this->archivoTerminos, &ss, offsetDeTermino, tamanioDato + 1);
+	this->archivoTerminos.flush();
+	//cout<<"EL ARC DE TERMINOS ESCRIBE: "<< ss.str()<<endl;
 
-	stringstream ss1;
-	ss1 << aux;
-
-	cout << ss1.str() << endl;
-	ManejadorArchivo::Escribir(this->archivoTerminos, &ss,offsetDeTermino);
-
-	return (uint32_t) offsetDeTermino;
+	return idTermino;
 }
 
-string ArchivoTerminos::obtenerTermino(uint32_t idTermino)
+std::string ArchivoTerminos::obtenerTermino(uint32_t idTermino)
 {
-	string termino;
-	string aux;
+	std::string termino;
+	uint32_t tamanioDeArchivo = ManejadorArchivo::GetSizeArchivo(this->archivoTerminos);
 	uint32_t PRR = idTermino; //Posicion de Registro Relativo de archivo.
 	bool finTermino = false;
+	char letraActual;
 
-	ManejadorArchivo::IrAlInicio(this->archivoTerminos);
-	ManejadorArchivo::PosicionarPunteroLecturaEn(this->archivoTerminos, PRR);
+	stringstream ss;
 
-	while(!finTermino)
+	if( PRR < tamanioDeArchivo)
 	{
-		//cout << "***" << idTermino << endl;
-		aux = ManejadorArchivo::LeerCaracteres(this->archivoTerminos,1);
-		//cout << "==== " << idTermino << endl;
-		if((int)aux[0] != 124)
-			termino += aux;
-		else
-			finTermino = true;
+		ManejadorArchivo::PosicionarPunteroLecturaEn(this->archivoTerminos, PRR);
+
+		while (!finTermino && PRR < tamanioDeArchivo)
+		{
+			//letraActual = ManejadorArchivo::LeerCaracteres(this->archivoTerminos, 1).at(0);
+
+			this->archivoTerminos.read(&letraActual, 1);
+
+			if (letraActual == FIN_DE_TERMINO)
+			{
+				finTermino = true;
+			}
+			else
+			{
+				ss.write(&letraActual, 1);
+			}
+
+			PRR = this->archivoTerminos.tellg();
+		}
 	}
+
+	termino = ss.str();
 	return termino;
 }
 
-vector<pair<string, uint32_t> > ArchivoTerminos::obtenerTerminos()
+std::vector<std::pair<std::string, uint32_t> > ArchivoTerminos::obtenerTerminos()
 {
-	//Para dejar el archivo en la misma posición, tras terminar el método.
-	uint32_t offsetPrevio = this->archivoTerminos.tellg();
-
-	uint32_t PRR = 0; // PRR = Posicion de Registro Relativo en archivo.
+	uint32_t PRR = 0; //puntero local
 	uint32_t tamanioArchivoTerminos = ManejadorArchivo::GetSizeArchivo(this->archivoTerminos);
 
-	ManejadorArchivo::IrAlInicio(this->archivoTerminos);
+	std::vector<std::pair<std::string, uint32_t> > listaTerminos;
+	pair<string, uint32_t> elementoTermino;
 	string termino;
 
-	vector<pair<string, uint32_t> > listaTerminos;
-
-	pair<string, uint32_t> elementoTermino;
-
-	//cout<<"arranco... PRR = "<<PRR<<"    Tamanio archivo: "<<tamanioArchivoTerminos<<endl;
-
 	while( PRR < tamanioArchivoTerminos)
 	{
-		//cout<<"PRR (ID): "<<PRR<<endl;
 		termino = this->obtenerTermino(PRR);
-		//cout<<"Termino: "<<termino<<endl;
-
 		elementoTermino.first = termino;
-		elementoTermino.second = PRR; // PRR = idTermino
-
-		PRR = this->archivoTerminos.tellg();// + 1?; //tomamos la siguiente posicion, considerando el separador de terminos.
-
+		elementoTermino.second = PRR;
 		listaTerminos.push_back(elementoTermino);
-	}
-
-	this->archivoTerminos.seekg(offsetPrevio, ios::beg);
-	return listaTerminos;
-
-}
-
-/*
-vector<pair<string, uint32_t> > ArchivoTerminos::obtenerTerminos()
-{
-	//Para dejar el archivo en la misma posición, tras terminar el método.
-	uint32_t offsetPrevio = this->archivoTerminos.tellg();
-
-	uint32_t PRR = 0; //puntero local
-
-	vector<pair<string, uint32_t> > listaTerminos;
-
-	uint32_t tamanioArchivoTerminos = ManejadorArchivo::GetSizeArchivo(this->archivoTerminos);
-
-	ManejadorArchivo::PosicionarPunteroLecturaEn(this->archivoTerminos, 0);
-
-	string palabra = "";
-	string letra = "";
-	char* letraChar;
-	uint32_t offsetTermino = 0;
-
-	pair<string, uint32_t> elementoTermino;
-
-	while( PRR < tamanioArchivoTerminos)
-	{
-		//este metodo de la clase leerCaracteres no anda bien, o se pisa, que se yo que corno le pasa
-		//letraChar = ManejadorArchivo::LeerCaracteres(this->archivoTerminos, 1).at(0); //aca lee basura... ??
-
-		letraChar = new char[1];
-		this->archivoTerminos.read(letraChar, 1);
-
-		if(*letraChar != FIN_DE_TERMINO)
-		{
-			letra = letraChar; //error en el valgrind. var. no inicializada
-			palabra.append(letra);
-		}
-		else
-		{
-			if(palabra.size() > 0)
-			{
-				elementoTermino.first = palabra;
-				elementoTermino.second = offsetTermino;
-				listaTerminos.push_back(elementoTermino);
-			}
-			palabra = "";
-			offsetTermino = PRR + 1;
-		}
-		delete[] letraChar;
 		PRR = this->archivoTerminos.tellg();
 	}
 
-	this->archivoTerminos.seekg(offsetPrevio, ios::beg);
 	return listaTerminos;
 }
-*/
