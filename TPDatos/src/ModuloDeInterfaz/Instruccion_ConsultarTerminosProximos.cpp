4b1d47c1983a1d8b02d641cbe182c9157588efc9
jimena.soledad.tapia@gmail.com <jimena.soledad.tapia@gmail.com@4d5b8590-2f34-da2a-ecbf-15d6c6d80ed1>
2011-06-21 09:28:52 +0000

    quita de stop words en la consulta de terminos proximos

--- TPDatos/src/ModuloDeInterfaz/Instruccion_ConsultarTerminosProximos.cpp
+++ TPDatos/src/ModuloDeInterfaz/Instruccion_ConsultarTerminosProximos.cpp
@@ -26,8 +26,29 @@ void Instruccion_ConsultarTerminosProximos::ejecutar()
 			"Se ejecuta la consulta.");
 
 	cout<<"Se devuelven los datos por terminos proximos de "<<this->titulo<<endl;
-	if(consulta->esConsultable())
-		consulta->consultarTerminosProximos(caseFold.caseFoldWord(titulo));
+	if(consulta->esConsultable()){
+
+		DatoLibro* datoLibro = new DatoLibro(caseFold.caseFoldWord(titulo));
+		Libro* libro = new Libro();
+		ParserDeOcurrenciasDeTerminos* parser = new ParserDeOcurrenciasDeTerminos(conf->getPathArchivoStopWords());
+		libro = parser->parsear(datoLibro);
+
+		string consultaTerminos = "";
+		map<string, int>::const_iterator it_map;
+
+		for(it_map=libro->getPalabrasClave()->begin();it_map!=libro->getPalabrasClave()->end();++it_map){
+			consultaTerminos = (*it_map)->first;
+			consultaTerminos = " ";
+
+		}
+		consulta->consultarTerminosProximos(consultaTerminos);
+
+		delete(datoLibro);
+		delete(libro);
+		delete(parser);
+
+	}
+
 	else
 		cout<<MENSAJE_INDICE_NO_CONSULTABLE<<endl;
 
